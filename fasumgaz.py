956e6f21fcb505c01c98fde1ddcd36b471ce5563
chetverovod <chetverovod@gmail.com>
2023-01-08 01:13:16 +0300

    Files updated.

--- fasumgaz.py
+++ fasumgaz.py
@@ -1,47 +1,54 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import io
-import streamlit as st
+from fastapi import FastAPI
+from pydantic import BaseModel
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from io import StringIO
+
+class Item(BaseModel):
+    text: str
+
+app = FastAPI()
+
 
 model_name = "IlyaGusev/rugpt3medium_sum_gazeta"
-
-@st.cache(allow_output_mutation=True)
-def load_model():
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    return  AutoModelForCausalLM.from_pretrained(model_name), tokenizer
-
-def load_txt():
-    uploaded_file = st.file_uploader(label='**Выберите текст для распознавания:**')
-    if uploaded_file is not None:
-        # To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
-
-        # To read file as string:
-        string_data = stringio.read()
-        st.write(string_data)
-        return string_data
-    else:
-        return None
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
 
 def summarize(model, tokenizer, article_text):
+    """Function makes and returns summary of article_text.
+ 
+  Parameters
+  ----------
+     model:
+          Model object.
+     tokenizer:
+          Tokens generator.
+      article_text : str
+          Input text.
+
+  Returns
+  -------
+      str
+          Summary of input text.
+   """
+
     text_tokens = tokenizer(
-    article_text,
-    max_length=600,
-    add_special_tokens=False, 
-    padding=False,
-    truncation=True)["input_ids"]
+      article_text,
+      max_length = 600,
+      add_special_tokens = False, 
+      padding = False,
+      truncation = True
+      )["input_ids"]
+
     input_ids = text_tokens + [tokenizer.sep_token_id]
     input_ids = torch.LongTensor([input_ids])
 
     output_ids = model.generate(
-    input_ids=input_ids,
-    no_repeat_ngram_size=4,
-    max_new_tokens = 100
+      input_ids=input_ids,
+      no_repeat_ngram_size = 4,
+      max_new_tokens = 100
     )
 
     summary = tokenizer.decode(output_ids[0], skip_special_tokens=False)
@@ -49,20 +56,30 @@ def summarize(model, tokenizer, article_text):
     summary = summary.split(tokenizer.eos_token)[0]
     return summary  
 
-def print_summary(s):
-        st.write(s)
 
+@app.get("/")
+def root():
+    return {"message": "Fasumgaz application is online!"}
 
-model, tokenizer = load_model()
-
-
-st.title('Генератор кратких выводов из газетной статьи')
-txt = load_txt()
-
-if txt:
-    result = st.button('Анализировать')
-    if result:
-        sum = summarize(model, tokenizer, txt)
-        st.write('**Выводы:**')
-        print_summary(sum)
-
+@app.post("/predict/")
+def predict(item: Item):
+  """Method builds summary for article from newpaper. 
+  Model "IlyaGusev/rugpt3medium_sum_gazeta" is used.
+ 
+  Parameters
+  ----------
+  item: str
+         Input text of article.
+  
+  Returns
+  -------
+  str
+     Summary of article.
+  """
+  
+  txt = item.text 
+  res = "Error."
+  if txt:
+     sum = summarize(model, tokenizer, txt)
+     res = sum 
+  return res    
